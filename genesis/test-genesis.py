ad4c9a1a0ccf82c39d671de68a85687976c6bffb
Robert Valentine <paraboliclabs@gmail.com>
2025-01-30 02:47:01 -0500

    Add genesis test file

---
+++ genesis/test-genesis.py
@@ -0,0 +1,24 @@
+import platform
+import genesis as gs
+gs.init(backend=gs.cpu)
+
+
+
+if platform.system() == "Windows":
+    print("Running on Windows")
+    scene = gs.Scene(show_viewer=False)
+elif platform.system() == "Linux":
+    print("Running on Linux")
+    scene = gs.Scene(show_viewer=True)
+else:
+    print("Running on another operating system")
+
+plane = scene.add_entity(gs.morphs.Plane())
+franka = scene.add_entity(
+    gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'),
+)
+
+scene.build()
+
+for i in range(1000):
+    scene.step()
